1aad1501ecd9b7b4b397da06c8a9f770a4c98114
grk <grkdesign@gmail.com>
2018-09-06 15:30:20 +0300

    Add dynamic output to homepage-blog.php

--- partials/homepage-blog.php
+++ partials/homepage-blog.php
@@ -1,63 +1,63 @@
 <div id="fh5co-blog" class="blog-flex">
-	<div class="featured-blog" style="background-image: url(images/blog-1.jpg);">
-		<div class="desc-t">
-			<div class="desc-tc">
-				<span class="featured-head">Featured Posts</span>
-				<h3><a href="#">Top 20 Best WordPress Themes 2017 Multi Purpose and Creative Websites</a></h3>
-				<span><a href="#" class="read-button">Learn More</a></span>
+	<?php
+	$hpid = $post->ID;
+	// dump($hpid);
+
+	$param = [
+		'cat' => get_field('hb_category'),
+		'posts_per_page' => get_field('hb_limit')
+	];
+
+	$result = new WP_Query($param);
+
+	if($result->have_posts()):
+		while($result->have_posts()):
+			$result->the_post();
+			?>
+			<div class="featured-blog" style="background-image: url(<?php the_post_thumbnail_url('medium-large'); ?>);">
+				<div class="desc-t">
+					<div class="desc-tc">
+						<span class="featured-head"><?php the_field('hb_featured_title', $hpid); ?></span>
+						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						<span><a href="<?php the_permalink(); ?>" class="read-button"><?php _e('Learn More', 'vcs-starter'); ?></a></span>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
+			<?php
+			break; //Nutraukiame cikla po pirmos iteracijos
+		endwhile; // end while
+	endif; // end if
+	?>
 	<div class="blog-entry fh5co-light-grey">
 		<div class="row animate-box">
 			<div class="col-md-12">
-				<h2>Latest Posts</h2>
+				<h2><?php the_field('hb_list_title', $hpid); ?></h2>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-12 animate-box">
-				<a href="#" class="blog-post">
-					<span class="img" style="background-image: url(images/blog-2.jpg);"></span>
-					<div class="desc">
-						<h3>26 Best Education WordPress Themes 2017 You Need To See</h3>
-						<span class="cat">Collection</span>
+			<?php
+			if($result->have_posts()):
+				while($result->have_posts()):
+					$result->the_post();
+					?>
+					<div class="col-md-12 animate-box">
+						<div class="blog-post">
+							<a href="<?php the_permalink(); ?>">
+								<span class="img" style="background-image: url(<?php the_post_thumbnail_url('thumbnail'); ?>);"></span>
+							</a>
+							<div class="desc">
+								<a href="<?php the_permalink(); ?>">
+									<h3><?php the_title(); ?></h3>
+								</a>
+								<span class="cat"><?php echo get_the_category_list(', '); ?></span>
+							</div>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col-md-12 animate-box">
-				<a href="#" class="blog-post">
-					<span class="img" style="background-image: url(images/blog-1.jpg);"></span>
-					<div class="desc">
-						<h3>16 Outstanding Photography WordPress Themes You Must See</h3>
-						<span class="cat">Collection</span>
-					</div>
-				</a>
-			</div>
-			<div class="col-md-12 animate-box">
-				<a href="#" class="blog-post">
-					<span class="img" style="background-image: url(images/blog-3.jpg);"></span>
-					<div class="desc">
-						<h3>16 Outstanding Photography WordPress Themes You Must See</h3>
-						<span class="cat">Collection</span>
-					</div>
-				</a>
-			</div><div class="col-md-12 animate-box">
-				<a href="#" class="blog-post">
-					<span class="img" style="background-image: url(images/blog-3.jpg);"></span>
-					<div class="desc">
-						<h3>16 Outstanding Photography WordPress Themes You Must See</h3>
-						<span class="cat">Collection</span>
-					</div>
-				</a>
-			</div><div class="col-md-12 animate-box">
-				<a href="#" class="blog-post">
-					<span class="img" style="background-image: url(images/blog-3.jpg);"></span>
-					<div class="desc">
-						<h3>16 Outstanding Photography WordPress Themes You Must See</h3>
-						<span class="cat">Collection</span>
-					</div>
-				</a>
-			</div>
+					<?php
+				endwhile; // end while
+			endif; // end if
+			wp_reset_postdata();
+			?>
 		</div>
 	</div>
 </div>
