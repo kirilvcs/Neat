86767585412ee0ca28b08b47bd973e6ca9e69495
grk <grkdesign@gmail.com>
2018-09-06 15:10:21 +0300

    Add dynamic output to hero.php

--- partials/hero.php
+++ partials/hero.php
@@ -1,62 +1,57 @@
 <aside id="fh5co-hero">
 	<div class="flexslider">
 		<ul class="slides">
-	   	<li style="background-image: url(images/img_bg_1.jpg);">
-	   		<div class="overlay-gradient"></div>
-	   		<div class="container-fluid">
-	   			<div class="row">
-		   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 slider-text">
-		   				<div class="slider-text-inner">
-		   					<h1>Not Every Project Needs To Be Perfect</h1>
-								<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-								<p><a class="btn btn-primary btn-demo" href="#"> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
-		   				</div>
-		   			</div>
-		   		</div>
-	   		</div>
-	   	</li>
-	   	<li style="background-image: url(images/img_bg_2.jpg);">
-	   		<div class="overlay-gradient"></div>
-	   		<div class="container-fluid">
-	   			<div class="row">
-		   			<div class="col-md-6 col-md-offset-3 col-md-push-3 slider-text">
-		   				<div class="slider-text-inner">
-		   					<h1>WordPress Theme For People Who Tell Stories</h1>
-								<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-								<p><a class="btn btn-primary btn-demo" href="#"> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
-		   				</div>
-		   			</div>
-		   		</div>
-	   		</div>
-	   	</li>
-	   	<li style="background-image: url(images/img_bg_3.jpg);">
-	   		<div class="overlay-gradient"></div>
-	   		<div class="container-fluids">
-	   			<div class="row">
-		   			<div class="col-md-6 col-md-offset-3 slider-text">
-		   				<div class="slider-text-inner text-center">
-		   					<h1>What Would You Like To Learn?</h1>
-								<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-								<p><a class="btn btn-primary btn-demo" href="#"> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
-		   				</div>
-		   			</div>
-		   		</div>
-	   		</div>
-	   	</li>
-	   	<li style="background-image: url(images/img_bg_4.jpg);">
-	   		<div class="overlay-gradient"></div>
-	   		<div class="container-fluid">
-	   			<div class="row">
-		   			<div class="col-md-6 col-md-offset-3 col-md-push-3 slider-text">
-		   				<div class="slider-text-inner">
-		   					<h1>I Love to Tell My Story</h1>
-								<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-								<p><a class="btn btn-primary btn-demo" href="#"> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
-		   				</div>
-		   			</div>
-		   		</div>
-	   		</div>
-	   	</li>	   	
+			<?php
+			if(have_rows('hs_slides_repeater')):
+				while(have_rows('hs_slides_repeater')): the_row();
+					$align = get_sub_field('slide_alignment');
+					if($align){
+						$text_align = '';
+					}else{
+						$align = '';
+						$text_align = 'text-center';
+					}
+					?>
+					<li style="background-image: url(<?php echo get_sub_field('slide_background')['sizes']['slider-image']; ?>);">
+				   		<div class="overlay-gradient"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 slider-text <?php echo $align; ?>">
+					   				<div class="slider-text-inner <?php echo $text_align; ?>">
+					   					<?php 
+					   					echo do_shortcode(get_sub_field('slide_content'));
+					   					?>
+										<p>
+											<?php
+												if(have_rows('slide_buttons_repeater')):
+													while(have_rows('slide_buttons_repeater')): the_row();
+														$link = get_sub_field('link');
+														//dump($link);
+														if($link['target']=="_blank"){
+															$link['target']='target="_blank"';
+														}else{
+															$link['target']='';
+														}
+														$link_class = get_sub_field('link_style');
+														if(!$link_class){
+															$link_class = '';
+														}
+														?>
+														<a class="btn btn-primary <?php echo $link_class; ?>" href="<?php echo $link['url'] ?>" <?php echo $link['target']; ?>><?php echo $link['title']; ?></a>
+														<?php
+													endwhile;
+												endif;
+											?>
+										</p>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+					<?php
+				endwhile;
+			endif;
+			?>
 	  	</ul>
   	</div>
 </aside>
