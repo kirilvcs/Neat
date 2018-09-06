c4d915cc00948eafaceda106e45fd6823ab9f85e
grk <grkdesign@gmail.com>
2018-09-06 15:21:33 +0300

    Add dynamic output to services

--- partials/services.php
+++ partials/services.php
@@ -1,37 +1,23 @@
 <div id="fh5co-services">
 	<div class="row">
-		<div class="col-md-4 text-center animate-box">
-			<div class="services">
-				<span class="icon">
-					<i class="icon-diamond"></i>
-				</span>
-				<div class="desc">
-					<h3><a href="#">Brand Identity</a></h3>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+		<?php
+		if(have_rows('hs_services_repeater')):
+			while(have_rows('hs_services_repeater')): the_row();
+				?>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<?php the_sub_field('icon'); ?>
+						</span>
+						<div class="desc">
+							<h3><a href="#"><?php the_sub_field('heading'); ?></a></h3>
+							<p><?php echo nl2br(get_sub_field('description')); ?></p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="col-md-4 text-center animate-box">
-			<div class="services">
-				<span class="icon">
-					<i class="icon-lab2"></i>
-				</span>
-				<div class="desc">
-					<h3><a href="#">Web Design &amp; UI</a></h3>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 text-center animate-box">
-			<div class="services">
-				<span class="icon">
-					<i class="icon-settings"></i>
-				</span>
-				<div class="desc">
-					<h3><a href="#">Web Development</a></h3>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
-		</div>
+				<?php
+			endwhile;
+		endif;
+		?>
 	</div>
 </div>
