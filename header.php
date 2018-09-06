6360c1392693c5db1b829cbfa5e7c5dfaee91c75
grk <grkdesign@gmail.com>
2018-09-06 14:56:30 +0300

    Add title to header.php

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
 	<head>
-	<title>Neat &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="<?php bloginfo("charset"); ?>">
+		<title><?php wp_title(); ?></title>
 
 
 
