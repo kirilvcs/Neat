c167a62618cfd3595aef099f3f768fbc92e900f6
grk <grkdesign@gmail.com>
2018-09-06 15:29:58 +0300

    Add homepage-blog.php call to homepage.php

--- templates/homepage.php
+++ templates/homepage.php
@@ -10,6 +10,7 @@ get_header();
 	get_template_part('partials/hero'); // uzkrauname hero.php is partials katalogo
 	get_template_part('partials/services');
 	get_template_part('partials/counter');
+	get_template_part('partials/homepage-blog');
 ?>
 
 <?php get_footer(); ?>
