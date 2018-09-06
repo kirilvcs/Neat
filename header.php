0b4725a682031b2b44bc9d7c5f45edf64b23f9aa
grk <grkdesign@gmail.com>
2018-09-06 15:07:15 +0300

    Add menu output to header.php

--- header.php
+++ header.php
@@ -26,21 +26,6 @@
 			<div class="top-menu">
 				<div class="row">
 					<div class="col-xs-10 text-right menu-1">
-						<ul>
-							<li class="active"><a href="index.html">Home</a></li>
-							<li><a href="work.html">Work</a></li>
-							<li class="has-dropdown">
-								<a href="blog.html">Blog</a>
-								<ul class="dropdown">
-									<li><a href="#">Web Design</a></li>
-									<li><a href="#">eCommerce</a></li>
-									<li><a href="#">Branding</a></li>
-									<li><a href="#">API</a></li>
-								</ul>
-							</li>
-							<li><a href="about.html">About</a></li>
-							<li><a href="contact.html">Contact</a></li>
-						</ul>
 							<div id="fh5co-logo">
 								<a href="<?php echo home_url(); ?>">
 									<?php
@@ -61,6 +46,16 @@
 									?>
 								</a>
 							</div>
+							<?php 
+							$args = [
+								"menu_class" => "surasome reikalingas klases",
+								"container" => false,
+								"theme_location" => 'primary-navigation'
+							];
+
+							wp_nav_menu($args);
+
+							?>
 					</div>
 				</div>
 				
