51d8e5b43a323055c83544da038e48a3c5ba9627
grk <grkdesign@gmail.com>
2018-09-07 15:32:27 +0300

    Add projects.php dynamic output

--- partials/projects.php
+++ partials/projects.php
@@ -1,34 +1,39 @@
 <div id="fh5co-work" class="fh5co-light-grey">
 	<div class="row animate-box">
 		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
-			<h2>Work</h2>
-			<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+			<h2><?php the_field('hp_section_heading'); ?></h2>
+			<?php echo do_shortcode(get_field('hp_section_description')); ?>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-4 text-center animate-box">
-			<a href="work-single.html" class="work"  style="background-image: url(images/portfolio-1.jpg);">
-				<div class="desc">
-					<h3>Project Name</h3>
-					<span>Illustration</span>
+		<?php 
+		//dump(get_field('hp_projects'));
+		$projects = get_field('hp_projects');
+
+		if($projects):
+			foreach($projects as $post):
+				setup_postdata($post);
+				//dump($post->ID);
+				//dump(wp_get_object_terms($post->ID, 'project-category'));
+				$categories = wp_get_object_terms($post->ID, 'project-category');
+				$category_list = [];
+				foreach($categories as $category){
+					$category_list[] = $category->name; //sukursime masyva su kategoriju sarasu
+				}
+				$category_list = implode(', ', $category_list); //padarome is masyvo tekstine eilute
+				?>
+				<div class="col-md-4 text-center animate-box">
+					<a href="<?php the_permalink(); ?>" class="work"  style="background-image: url(<?php the_post_thumbnail_url('medium-large');?>);">
+						<div class="desc">
+							<h3><?php the_title(); ?></h3>
+							<span><?php echo $category_list; ?></span>
+						</div>
+					</a>
 				</div>
-			</a>
-		</div>
-		<div class="col-md-4 text-center animate-box">
-			<a href="work-single.html" class="work" style="background-image: url(images/portfolio-2.jpg);">
-				<div class="desc">
-					<h3>Project Name</h3>
-					<span>Brading</span>
-				</div>
-			</a>
-		</div>
-		<div class="col-md-4 text-center animate-box">
-			<a href="work-single.html" class="work" style="background-image: url(images/portfolio-3.jpg);">
-				<div class="desc">
-					<h3>Project Name</h3>
-					<span>Illustration</span>
-				</div>
-			</a>
-		</div>
+				<?php
+				wp_reset_postdata();
+			endforeach;
+		endif;
+		?>
 	</div>
 </div>
